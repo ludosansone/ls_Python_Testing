f72866f6ef33b06d3f6d9904027c166b3e2633e2
Ludovic SANSONE <libreaudio63@gmail.com>
2023-06-06 14:40:03 +0200

    Correction de la fonction purchasePlaces

--- server.py
+++ server.py
@@ -56,13 +56,14 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
+    now = str(datetime.now())
     if placesRequired <= 12\
        and placesRequired <= int(competition['numberOfPlaces'])\
        and placesRequired <= int(club['points']):
         competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
         club['points'] = int(club['points']) - placesRequired
         flash('Great-booking complete!')
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=club, competitions=competitions, now=now)
     else:
         flash('Invalid booking')
         return redirect(url_for('book', competition=competition['name'], club=club['name']))
